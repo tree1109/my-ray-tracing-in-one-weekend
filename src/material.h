a914f89e21b47b4d5a8a62307d3740f1f60cb715
tree1109 <55215480+tree1109@users.noreply.github.com>
2026-05-09 00:37:53 +0800

    ch 11.4

--- src/material.h
+++ src/material.h
@@ -69,7 +69,7 @@ public:
         bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract) {
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
             direction = reflect(unit_direction, record.normal);
         }
         else {
@@ -84,6 +84,13 @@ private:
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media
     double refraction_index;
+
+    static double reflectance(const double cosine, const double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
 };
 
 #endif // INCLUDE_MATERIAL_H
