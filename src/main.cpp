3c06bd7a2adecacb989bf73a271e095a288cdc63
tree1109 <55215480+tree1109@users.noreply.github.com>
2026-05-09 16:24:57 +0800

    ch 13.2

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,9 @@ namespace {
         cam.lookfrom = point3(-2,2,1);
         cam.lookat   = point3(0,0,-1);
         cam.vup      = vec3(0,1,0);
+
+        cam.defocus_angle = 10.0;
+        cam.focus_dist = 3.4;
     }
 }
 
--- src/vec3.h
+++ src/vec3.h
@@ -112,6 +112,14 @@ inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1.0, 1.0), random_double(-1.0, 1.0), 0);
+        if (p.length_squared() < 1.0)
+            return p;
+    }
+}
+
 inline vec3 random_unit_vector() {
     while (true) {
         auto p = vec3::random(-1, 1);
